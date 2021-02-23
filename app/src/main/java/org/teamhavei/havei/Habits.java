510b73220c44865f05fdcb02296ba8b093ff7aab
lizbaka <1849214604@qq.com>
2021-02-23 21:17:50 +0800

    added annotation

--- app/src/main/java/org/teamhavei/havei/Habits.java
+++ app/src/main/java/org/teamhavei/havei/Habits.java
@@ -6,11 +6,11 @@ public class Habits {
     public static final int FREQUENCY_PER_WEEK = 2;
     public static final int FREQUENCY_PER_MONTH = 3;
 
-    private String habitName;
-    private String habitTag;
-    private int habitFrequency;
-    private int habitFrequencyPer;
-    private int habitExecTimes;
+    private String habitName;//名称
+    private String habitTag;//标签
+    private int habitFrequency;//频率：次数
+    private int habitFrequencyPer;//频率：时间单位
+    private int habitExecTimes;//已执行次数
 
     public Habits(String habitName, String habitTag, int habitFrequency, int habitFrequencyPer, int habitExecTimes) {
         this.habitName = habitName;
