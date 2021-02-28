d97eb190fac73a0f94107ccd3beb896e30ab17f0
lizbaka <1849214604@qq.com>
2021-02-28 18:34:15 +0800

    增加了toolbar，navigationview

--- app/src/main/java/org/teamhavei/havei/activities/ActivityMain.java
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityMain.java
@@ -1,11 +1,16 @@
 package org.teamhavei.havei.activities;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -18,41 +23,94 @@ import java.util.List;
 
 public class ActivityMain extends AppCompatActivity {
 
-    private String[] titles = new String[] {"记账","习惯","备忘"};
+    ViewPager mViewPager;
+    Toolbar mToolbar;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
+    DrawerLayout mDrawerLayout;
+    TabLayout mTabLayout;
 
-    private int defaultSelectedTab = 1;
+    private String[] tabTitles = new String[] {"记账","习惯","备忘"};
+
+    private int defaultPage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initTabAndPager();
+        initDrawer();
+        initToolbar();
+        initPager();
     }
 
+    private void initDrawer(){
 
-    private void initTabAndPager(){
+        mDrawerLayout = findViewById(R.id.main_drawer);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.main_view_pager);
+    }
 
+    private void initPager(){
+
+        mViewPager = (ViewPager) findViewById(R.id.main_view_pager);
         List<Fragment> fragmentList = new ArrayList<>();
         fragmentList.add(new FragmentAccount());
 //        fragmentList.add(new FragmentDashBoard());
-        fragmentList.add(new FragmentHabits());
+        fragmentList.add(new FragmentHabit());
         fragmentList.add(new FragmentNote());
-
-        for(int i=0;i<titles.length;i++){
-            tabLayout.addTab(tabLayout.newTab());
+        mViewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(),fragmentList, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
+        mViewPager.setCurrentItem(defaultPage);
+        mTabLayout = findViewById(R.id.main_tab);
+        for(int i=0;i<tabTitles.length;i++){
+            mTabLayout.addTab(mTabLayout.newTab());
         }
-
-        tabLayout.setupWithViewPager(viewPager,false);
-        viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(),fragmentList, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
-
-        for(int i=0;i<titles.length;i++){
-            tabLayout.getTabAt(i).setText(titles[i]);
+        mTabLayout.setupWithViewPager(mViewPager,false);
+        for(int i=0;i<tabTitles.length;i++){
+            mTabLayout.getTabAt(i).setText(tabTitles[i]);
         }
-        tabLayout.getTabAt(defaultSelectedTab).select();
     }
+
+    private void initToolbar(){
+
+        mToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,R.string.drawer_open, R.string.drawer_close);
+        mActionBarDrawerToggle.setDrawerSlideAnimationEnabled(true);
+        mActionBarDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        getSupportActionBar().setTitle("");
+        mToolbar.inflateMenu(R.menu.toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+//    private void initToolbarAndPager(){
+//
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab);
+//
+//
+//        List<Fragment> fragmentList = new ArrayList<>();
+//        fragmentList.add(new FragmentAccount());
+////        fragmentList.add(new FragmentDashBoard());
+//        fragmentList.add(new FragmentHabit());
+//        fragmentList.add(new FragmentNote());
+//
+//        for(int i=0;i<tabTitles.length;i++){
+//            tabLayout.addTab(tabLayout.newTab());
+//        }
+//
+//        tabLayout.setupWithViewPager(viewPager,false);
+//        viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(),fragmentList, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
+//
+//        for(int i=0;i<tabTitles.length;i++){
+//            tabLayout.getTabAt(i).setText(tabTitles[i]);
+//        }
+//        tabLayout.getTabAt(defaultSelectedTab).select();
+//    }
 
 }
--- app/src/main/java/org/teamhavei/havei/fragments/FragmentHabit.java
+++ app/src/main/java/org/teamhavei/havei/fragments/FragmentHabit.java
@@ -16,7 +16,7 @@ import org.teamhavei.havei.adapters.HabitCardAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FragmentHabits extends Fragment {
+public class FragmentHabit extends Fragment {
 
     private List<Habit> habitList;
 
@@ -29,7 +29,7 @@ public class FragmentHabits extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-        View view = inflater.inflate(R.layout.fragment_habits, container, false);
+        View view = inflater.inflate(R.layout.fragment_habit, container, false);
         RecyclerView habitCardList = (RecyclerView) view.findViewById(R.id.habit_card_list);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         habitCardList.setLayoutManager(layoutManager);
