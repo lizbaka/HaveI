50ae49b0e03ad60db72547f7b430a7f7e8c267a1
lizbaka <1849214604@qq.com>
2021-03-14 14:16:27 +0800

    引入baseactivity

---
+++ app/src/main/java/org/teamhavei/havei/activities/BaseActivity.java
@@ -0,0 +1,9 @@
+package org.teamhavei.havei.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    public static final String TAG = "DEBUG";
+
+}
