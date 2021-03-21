7c30840243a528a5358207265242c660cf3d3ae3
lizbaka <1849214604@qq.com>
2021-03-21 22:22:05 +0800

    提供了查询习惯信息的接口

--- app/src/main/java/org/teamhavei/havei/habit/HabitDBHelper.java
+++ app/src/main/java/org/teamhavei/havei/habit/HabitDBHelper.java
@@ -1,16 +1,21 @@
 /*可能需要的数据表
-* Habit(id,integer name text, tag text)
-* HabitExecs(name text, date date)
+* Habit(id integer, name text, tag text)
+* HabitExecs(habit_id integer, date date)
  */
 package org.teamhavei.havei.habit;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class HabitDBHelper extends SQLiteOpenHelper {
+
+    public static final String TAG = "DEBUG";
 
     public static final int DATABASE_VERSION = 1;
     public static final String DB_NAME = "Habit.db";
@@ -21,13 +26,12 @@ public class HabitDBHelper extends SQLiteOpenHelper {
                     "name text," +
                     "tag text)";
 
-    private static final String CREATE_HABITEXECS =
+    private static final String CREATE_HABIT_EXECS =
             "create table HabitExecs(" +
-                    "name text," +
+                    "habit_id integer," +
                     "date datetime)";
 
     private Context mContext;
-
 
     public HabitDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -37,10 +41,71 @@ public class HabitDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_HABIT);
-        db.execSQL(CREATE_HABITEXECS);
+        db.execSQL(CREATE_HABIT_EXECS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+
+    public int getHabitID(String habitName){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query("Habit",new String[]{"id"},"name = ?",new String[]{habitName},null,null,null);
+        cursor.moveToFirst();
+        int id = 0;
+        try{
+            id = cursor.getInt(cursor.getColumnIndex("id"));
+        }catch (CursorIndexOutOfBoundsException e){
+            e.printStackTrace();
+            Log.e(TAG, "getHabitID: habitName not found", e);
+        }
+        cursor.close();
+        return id;
+    }
+
+    public String getHabitName(int ID){
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.query("Habit",new String[]{"name"},"id = ?",new String[]{Integer.toString(ID)},null,null,null);
+        cursor.moveToFirst();
+        String habitName = "";
+        try{
+            habitName = cursor.getString(cursor.getColumnIndex("name"));
+        }catch (CursorIndexOutOfBoundsException e){
+            e.printStackTrace();
+            Log.e(TAG, "getHabitName: habitID not found", e);
+        }
+        cursor.close();
+        return habitName;
+    }
+
+    public String getHabitTag(int ID){
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.query("Habit",new String[]{"tag"},"id = ?",new String[]{Integer.toString(ID)},null,null,null);
+        cursor.moveToFirst();
+        String habitName = "";
+        try{
+            habitName = cursor.getString(cursor.getColumnIndex("tag"));
+        }catch (CursorIndexOutOfBoundsException e){
+            e.printStackTrace();
+            Log.e(TAG, "getHabitTag: habitID not found", e);
+        }
+        cursor.close();
+        return habitName;
+    }
+
+    public String getHabitTag(String habitName){
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.query("Habit",new String[]{"tag"},"name = ?",new String[]{habitName},null,null,null);
+        cursor.moveToFirst();
+        String habitTag = "";
+        try{
+            habitTag = cursor.getString(cursor.getColumnIndex("tag"));
+        }catch(CursorIndexOutOfBoundsException e){
+            e.printStackTrace();
+            Log.e(TAG, "getHabitTag: habitName not found", e);
+        }
+        cursor.close();
+        return habitTag;
     }
 }
