a4c655dcf3df92cddce75a0be06a88d685841d40
lizbaka <1849214604@qq.com>
2021-03-28 18:53:29 +0800

    为Habit提供了更多属性

--- app/src/main/java/org/teamhavei/havei/adapters/HabitCardAdapter.java
+++ app/src/main/java/org/teamhavei/havei/adapters/HabitCardAdapter.java
@@ -1,11 +1,12 @@
 package org.teamhavei.havei.adapters;
 
-import android.content.Intent;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,27 +14,42 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.teamhavei.havei.activities.ActivityHabitDetail;
 import org.teamhavei.havei.habit.Habit;
 import org.teamhavei.havei.R;
+import org.teamhavei.havei.habit.HabitDBHelper;
 
 import java.util.List;
 
 public class HabitCardAdapter extends RecyclerView.Adapter<HabitCardAdapter.ViewHolder> {
 
+    private static final String TAG = "DEBUG";
+
     private List<Habit> mHabitList;
+    private HabitDBHelper dbHelper;
+    private SQLiteDatabase db;
+    private Context mContext;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
+
         TextView name;
         TextView tag;
+        TextView done;
+        TextView count;
+        Habit mHabit;
 
         public ViewHolder(View view){
             super(view);
             name = (TextView) view.findViewById(R.id.habit_card_name);
             tag = (TextView) view.findViewById(R.id.habit_card_tag);
+            done = (TextView) view.findViewById(R.id.habit_card_done);
+            count = (TextView) view.findViewById(R.id.habit_card_count);
         }
 
     }
 
-    public HabitCardAdapter(List<Habit> habitList){
+    public HabitCardAdapter(List<Habit> habitList,Context context){
         mHabitList = habitList;
+        mContext = context;
+        dbHelper = new HabitDBHelper(mContext,HabitDBHelper.DB_NAME,null,HabitDBHelper.DATABASE_VERSION);
+        db = dbHelper.getWritableDatabase();
     }
 
     @NonNull
@@ -44,14 +60,19 @@ public class HabitCardAdapter extends RecyclerView.Adapter<HabitCardAdapter.View
         holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                ActivityHabitDetail.startAction(parent.getContext(),holder.name.getText().toString());
+                ActivityHabitDetail.startAction(parent.getContext(),holder.mHabit.getHabitName());
             }
         });
         holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                //增加执行记录
-                //动画
+                int habitID = holder.mHabit.getHabitID();
+                if(dbHelper.switchHabitExec(habitID)){
+                    holder.done.setText("太棒了！今天已经完成");
+                }
+                else{
+                    holder.done.setText("今天尚未完成");
+                }
                 return true;
             }
         });
@@ -60,9 +81,20 @@ public class HabitCardAdapter extends RecyclerView.Adapter<HabitCardAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Habit habit = mHabitList.get(position);
-        holder.name.setText(habit.getHabitName());
-        holder.tag.setText(habit.getHabitTag());
+        holder.mHabit = mHabitList.get(position);
+        String habitName = holder.mHabit.getHabitName();
+        String habitTag = holder.mHabit.getHabitTag();
+        int habitID = holder.mHabit.getHabitID();
+        holder.name.setText(habitName);
+        holder.tag.setText(habitTag);
+        int count = dbHelper.getHabitExecCount(habitID);
+        holder.count.setText("您已坚持完成习惯 " + count + " 次，持之以恒！");
+        if(dbHelper.isHabitDoneToday(habitID)){
+            holder.done.setText("太棒了！今天已经完成");
+        }
+        else{
+            holder.done.setText("今天尚未完成");
+        }
     }
 
     @Override
--- app/src/main/java/org/teamhavei/havei/habit/Habit.java
+++ app/src/main/java/org/teamhavei/havei/habit/Habit.java
@@ -2,8 +2,17 @@ package org.teamhavei.havei.habit;
 
 public class Habit {
 
+    private int habitID;
     private String habitName;//名称
     private String habitTag;//标签
+
+    public int getHabitID() {
+        return habitID;
+    }
+
+    public void setHabitID(int habitID) {
+        this.habitID = habitID;
+    }
 
     public String getHabitName() {
         return habitName;
