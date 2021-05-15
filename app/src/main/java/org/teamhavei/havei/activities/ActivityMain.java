08e0d5fafb74cd2ae9af3b07d7a45d2a89366184
lizbaka <1849214604@qq.com>
2021-05-15 18:30:37 +0800

    重构主页

--- app/src/main/java/org/teamhavei/havei/activities/ActivityMain.java
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityMain.java
@@ -27,23 +27,11 @@ import java.util.List;
 
 public class ActivityMain extends BaseActivity{
 
-    private static final int ACCOUNT_INDEX = 0;
-    private static final int DASHBOARD_INDEX = 1;
-    private static final int HABIT_INDEX = 2;
-    private static final int TODO_INDEX = 3;
-
-    private String[] pageTitle;
-    private int defaultFragment = 1;
-
     Toolbar mToolbar;
     ActionBarDrawerToggle mActionBarDrawerToggle;
     DrawerLayout mDrawerLayout;
-    TabLayout mTabLayout;
-    ViewPager mViewPager;
-    ExtendedFloatingActionButton fab;
     NavigationView mNavView;
 
-    View.OnClickListener addAccount, addAny, addHabit, addTodo;
 
     private List<Fragment> fragmentList = new ArrayList<>();
 
@@ -52,48 +40,9 @@ public class ActivityMain extends BaseActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initFABOnClickListeners();
-        fab = findViewById(R.id.main_fab);
-        initFragments();
         initDrawer();
         initToolbar();
-        initPager();
         initNavigationView();
-    }
-
-    void initFABOnClickListeners(){
-        addAccount = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ActivityMain.this,"功能：添加记账敬请期待",Toast.LENGTH_SHORT).show();
-            }
-        };
-        addAny = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ActivityMain.this,"添加任意项目功能：敬请期待",Toast.LENGTH_SHORT).show();
-            }
-        };
-        addHabit = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                FragmentHabit fragmentHabit = (FragmentHabit)fragmentList.get(HABIT_INDEX);
-                fragmentHabit.addHabit();
-            }
-        };
-        addTodo = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ActivityMain.this,"日程功能：敬请期待",Toast.LENGTH_SHORT).show();
-            }
-        };
-    }
-
-    void initFragments(){
-        fragmentList.add(new FragmentAccount());
-        fragmentList.add(new FragmentDashBoard());
-        fragmentList.add(new FragmentHabit());
-        fragmentList.add(new FragmentTodo());
     }
 
     private void initDrawer(){
@@ -110,49 +59,6 @@ public class ActivityMain extends BaseActivity{
         mActionBarDrawerToggle.setDrawerSlideAnimationEnabled(true);
         mActionBarDrawerToggle.syncState();
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
-        getSupportActionBar().setTitle("");
-    }
-
-    void initPager(){
-        pageTitle = getResources().getStringArray(R.array.main_page_title);
-        mViewPager = (ViewPager) findViewById(R.id.main_view_pager);
-        mTabLayout = (TabLayout) findViewById(R.id.main_tab_layout);
-        mTabLayout.setupWithViewPager(mViewPager);
-        mViewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(),fragmentList, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
-        for(int i=0;i<fragmentList.size();i++){
-            mTabLayout.getTabAt(i).setText(pageTitle[i]);
-        }
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch(position){
-                    case 0://account
-                        fab.setOnClickListener(addAccount);
-                        break;
-                    case 1://dashboard
-                        fab.setOnClickListener(addAny);
-                        break;
-                    case 2://habit
-                        fab.setOnClickListener(addHabit);
-                        break;
-                    case 3://note
-                        fab.setOnClickListener(addTodo);
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-        mViewPager.setCurrentItem(defaultFragment);
     }
 
     private void initNavigationView(){
