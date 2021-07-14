4f73e0efc63011948ed57e170f3d96f76f18b546
lizbaka <1849214604@qq.com>
2021-07-14 13:41:53 +0800

    准备新建Habit List界面

---
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityHabitList.java
@@ -0,0 +1,16 @@
+package org.teamhavei.havei.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.teamhavei.havei.R;
+
+public class ActivityHabitList extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_list);
+    }
+}
