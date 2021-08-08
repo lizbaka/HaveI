fcc3720b2ee7704d81ff485bb95d46be8a4b433c
Doki <3044208181@qq.com>
2021-08-08 09:08:54 +0800

    将资产一览、年度收支一览布局的工具条设置成了活动的应用条。

---
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityBookkeepAnnualAccountDetail.java
@@ -0,0 +1,20 @@
+package org.teamhavei.havei.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
+
+import org.teamhavei.havei.R;
+
+public class ActivityBookkeepAnnualAccountDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookkeep_annual_account_detail);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_bookkeep_annual_account_detail);
+        setSupportActionBar(toolbar);
+    }
+}
