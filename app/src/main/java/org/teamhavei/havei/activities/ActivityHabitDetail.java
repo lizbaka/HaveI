98b1f8082af7938a93ae786fa3d55cdc2d552bdc
lizbaka <1849214604@qq.com>
2021-08-10 12:52:37 +0800

    初步实现ActivityHabitDetail界面后端

--- app/src/main/java/org/teamhavei/havei/activities/ActivityHabitDetail.java
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityHabitDetail.java
@@ -1,152 +1,147 @@
 package org.teamhavei.havei.activities;
-//
-//import androidx.annotation.NonNull;
-//import androidx.appcompat.widget.Toolbar;
-//
-//import android.app.AlertDialog;
-//import android.content.Context;
-//import android.content.DialogInterface;
-//import android.content.Intent;
-//import android.database.Cursor;
-//import android.database.sqlite.SQLiteDatabase;
-//import android.os.Bundle;
-//import android.view.Menu;
-//import android.view.MenuItem;
-//import android.widget.Toast;
-//
-//import com.prolificinteractive.materialcalendarview.CalendarDay;
-//import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
-//import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
-//
-//import org.teamhavei.havei.R;
-//import org.teamhavei.havei.databases.EventDBHelper;
-//import org.threeten.bp.LocalDate;
-//import org.threeten.bp.format.DateTimeFormatter;
-//
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
+
+import org.teamhavei.havei.Event.Habit;
+import org.teamhavei.havei.Event.HabitExec;
+import org.teamhavei.havei.R;
+import org.teamhavei.havei.databases.EventDBHelper;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.List;
+
 public class ActivityHabitDetail extends BaseActivity {
-//
-//    private Toolbar mToolbar;
-//    private MaterialCalendarView mCalendar;
-//
-//    private EventDBHelper dbHelper;
-//    SQLiteDatabase db;
-//
-//    private String habitName;
-//    private int habitID;
-//
-//
-//    public static final String START_PARAM_HABIT_NAME = "habit_name";
-//    public static void startAction(Context context, String habitName){
-//        Intent intent = new Intent(context, ActivityHabitDetail.class);
-//        intent.putExtra(START_PARAM_HABIT_NAME,habitName);
-//        context.startActivity(intent);
-//    }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_habit_detail);
-//
-//        mToolbar = findViewById(R.id.habit_detail_toolbar);
-//        mToolbar.inflateMenu(R.menu.habit_detail_toolbar);
-//        setSupportActionBar(mToolbar);
-//        mCalendar = findViewById(R.id.habit_detail_calendar);
-//        dbHelper = new EventDBHelper(this, EventDBHelper.DB_NAME,null, EventDBHelper.DATABASE_VERSION);
-//        db = dbHelper.getWritableDatabase();
-//
-//        habitName = getIntent().getStringExtra(START_PARAM_HABIT_NAME);
-//        habitID = dbHelper.getHabitID(habitName);
-//
-//
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//        mCalendar.setOnDateLongClickListener(new OnDateLongClickListener() {
-//            @Override
-//            public void onDateLongClick(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date) {
-//                if(date.isAfter(CalendarDay.today())){
-//                    Toast.makeText(ActivityHabitDetail.this,getString(R.string.habit_detail_advanced),Toast.LENGTH_SHORT).show();
-//                    return;
-//                }
-//
-//                LocalDate localDate = date.getDate();
-//                String dateString = localDate.format(DateTimeFormatter.BASIC_ISO_DATE);
-//                boolean done = dbHelper.switchHabitExec(habitID,dateString);
-//                widget.setDateSelected(date,done);
-//                String toastMessage;
-//                if(done){
-//                    toastMessage = getString(R.string.habit_detail_marked);
-//                }
-//                else{
-//                    toastMessage = getString(R.string.habit_detail_unmarked);
-//                }
-//                Toast.makeText(ActivityHabitDetail.this,toastMessage,Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        habitName = dbHelper.getHabitName(habitID);
-//        getSupportActionBar().setTitle(habitName);
-//        showExecutionDate();
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.habit_detail_toolbar, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.habit_detail_toolbar_modify:
-//                ActivityModifyHabit.startAction(this,ActivityModifyHabit.MODE_MODIFY,habitName);
-//                return true;
-//            case R.id.habit_detail_toolbar_delete:
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setTitle(getString(R.string.habit_detail_delete_dialog_title));
-//                builder.setMessage(getString(R.string.habit_detail_delete_dialog_msg1) + habitName + getString(R.string.habit_detail_delete_dialog_msg2));
-//                builder.setPositiveButton(getString(R.string.dialog_positive), new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        deleteHabit();
-//                        finish();
-//                    }
-//                });
-//                builder.setNegativeButton(getString(R.string.dialog_negative), new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                    }
-//                });
-//                builder.setCancelable(true);
-//                builder.show();
-//                return true;
-//            case android.R.id.home:
-//                finish();
-//                return true;
-//        }
-//
-//        return false;
-//    }
-//
-//    private void showExecutionDate(){
-//        Cursor cursor = db.query("HabitExecs",new String[]{"date"},"habit_id = ?",new String[]{Integer.toString(habitID)},null,null,null);
-//        if(cursor.moveToFirst()){
-//            do {
-//                String dateString = cursor.getString(cursor.getColumnIndex("date"));
-//                int year = Integer.parseInt(dateString.substring(0,4));
-//                int month = Integer.parseInt(dateString.substring(4,6));
-//                int day = Integer.parseInt(dateString.substring(6,8));
-//                mCalendar.setDateSelected(CalendarDay.from(year,month,day),true);
-//            }while(cursor.moveToNext());
-//        }
-//        cursor.close();
-//    }
-//
-//    private void deleteHabit(){
-//        db.delete("HabitExecs","habit_id = ?",new String[]{Integer.toString(habitID)});
-//        db.delete("Habit","id = ?",new String[]{Integer.toString(habitID)});
-//    }
+
+    private static final String START_PARAM_HABIT_ID = "habit_id";
+    private static final int NULL_HABIT_ID = -1;
+
+    private Toolbar mToolbar;
+    private MaterialCalendarView mCalendar;
+
+    private EventDBHelper dbHelper;
+    List<HabitExec> habitExecList;
+
+    private Habit mHabit = null;
+
+    public static void startAction(Context context, Habit habit) {
+        Intent intent = new Intent(context, ActivityHabitDetail.class);
+        intent.putExtra(START_PARAM_HABIT_ID, habit.getId());
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_detail);
+
+        mToolbar = findViewById(R.id.habit_detail_toolbar);
+        mToolbar.inflateMenu(R.menu.habit_detail_toolbar);
+        setSupportActionBar(mToolbar);
+        mCalendar = findViewById(R.id.habit_detail_calendar);
+        dbHelper = new EventDBHelper(this, EventDBHelper.DB_NAME, null, EventDBHelper.DB_VERSION);
+        mHabit = dbHelper.findHabitById(getIntent().getIntExtra(START_PARAM_HABIT_ID, NULL_HABIT_ID));
+        habitExecList = dbHelper.findHabitExecByHabitId(mHabit.getId());
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mCalendar.setOnDateLongClickListener(new OnDateLongClickListener() {
+            @Override
+            public void onDateLongClick(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date) {
+                if (date.isAfter(CalendarDay.today())) {
+                    Toast.makeText(ActivityHabitDetail.this, getString(R.string.habit_detail_advanced), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                LocalDate localDate = date.getDate();
+                /* BASIC_ISO_DATE == yyyyMMdd */
+                String dateString = localDate.format(DateTimeFormatter.BASIC_ISO_DATE);
+                boolean done = dbHelper.switchHabitExec(mHabit.getId(), dateString);
+                widget.setDateSelected(date, done);
+                String toastMessage;
+                if (done) {
+                    toastMessage = getString(R.string.habit_detail_marked);
+                } else {
+                    toastMessage = getString(R.string.habit_detail_unmarked);
+                }
+                Toast.makeText(ActivityHabitDetail.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHabit = dbHelper.findHabitById(getIntent().getIntExtra(START_PARAM_HABIT_ID, NULL_HABIT_ID));
+        getSupportActionBar().setTitle(mHabit.getName());
+        showExecutionDate();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.habit_detail_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.habit_detail_toolbar_modify:
+                // TODO: 2021.08.10 完成ModifyHabit后接入
+//                ActivityModifyHabit.startAction(this, ActivityModifyHabit.MODE_MODIFY, mHabit.getName());
+                return true;
+            case R.id.habit_detail_toolbar_delete:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.habit_detail_delete_dialog_title));
+                builder.setMessage(getString(R.string.habit_detail_delete_dialog_msg1) + mHabit.getName() + getString(R.string.habit_detail_delete_dialog_msg2));
+                builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for(HabitExec i:habitExecList){
+                            dbHelper.deleteHabitExec(i);
+                        }
+                        dbHelper.deleteHabit(mHabit);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                builder.setCancelable(true);
+                builder.show();
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return false;
+    }
+
+    private void showExecutionDate() {
+        for (HabitExec i:habitExecList) {
+            String dateString = i.getDate();
+            int year = Integer.parseInt(dateString.substring(0, 4));
+            int month = Integer.parseInt(dateString.substring(4, 6));
+            int day = Integer.parseInt(dateString.substring(6, 8));
+            mCalendar.setDateSelected(CalendarDay.from(year, month, day), true);
+        }
+    }
 }
