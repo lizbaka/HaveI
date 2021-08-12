ae698775a5be4a8c5487148e5a230e27f85d267e
Doki <3044208181@qq.com>
2021-08-12 11:13:46 +0800

    给bookkeep布局的按钮加上了onClick属性

--- app/src/main/java/org/teamhavei/havei/activities/ActivityBookkeep.java
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityBookkeep.java
@@ -2,7 +2,9 @@ package org.teamhavei.havei.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import org.teamhavei.havei.R;
 
@@ -12,5 +14,17 @@ public class ActivityBookkeep extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookkeep);
+    }
+    public void onClickFindAnnualAccountDetail(View view){
+        Intent intent = new Intent(this,ActivityBookkeepAnnualAccountDetail.class);
+        startActivity(intent);
+    }
+    public void onClickFindStatisticsView(View view){
+        Intent intent = new Intent(this,ActivityBookkeepStatisticsView.class);
+        startActivity(intent);
+    }
+    public void onClickFindPropertyView(View view){
+        Intent intent = new Intent(this,ActivityBookkeepPropertyView.class);
+        startActivity(intent);
     }
 }
