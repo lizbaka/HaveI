9ef633908e1838ee6dab45323eccba4d1e3f2572
lizbaka <1849214604@qq.com>
2021-08-21 21:42:19 +0800

    完成习惯打卡记录界面开发

--- app/src/main/java/org/teamhavei/havei/activities/ActivityHabitMain.java
+++ app/src/main/java/org/teamhavei/havei/activities/ActivityHabitMain.java
@@ -1,6 +1,7 @@
 package org.teamhavei.havei.activities;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,16 +10,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import org.teamhavei.havei.R;
+import org.teamhavei.havei.fragments.FragmentHabitDoneRecordTable;
 import org.teamhavei.havei.fragments.FragmentHabitList;
 import org.teamhavei.havei.fragments.FragmentTodayHabit;
 
 public class ActivityHabitMain extends BaseActivity {
 
     BottomNavigationView navigationView;
+    TabLayout tabLayout;
 
     public static void startAction(Context context){
         Intent intent = new Intent(context, ActivityHabitMain.class);
@@ -40,15 +45,29 @@ public class ActivityHabitMain extends BaseActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
                     case R.id.habit_main_bottom_nav_today:
+                        tabLayout.setVisibility(View.GONE);
+                        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24_white);
+                        findViewById(R.id.habit_main_appbar).setBackgroundColor(ContextCompat.getColor(ActivityHabitMain.this,R.color.amber_500));
+
                         getSupportActionBar().setTitle(R.string.today_habit_title);
                         replaceFragment(new FragmentTodayHabit());
                         return true;
                     case R.id.habit_main_bottom_nav_lib:
+                        tabLayout.setVisibility(View.GONE);
+                        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24_white);
+                        findViewById(R.id.habit_main_appbar).setBackgroundColor(ContextCompat.getColor(ActivityHabitMain.this,R.color.amber_500));
+
+
                         getSupportActionBar().setTitle(R.string.habit_list_title);
                         replaceFragment(new FragmentHabitList());
                         return true;
                     case R.id.habit_main_bottom_nav_record:
-                        // TODO: 2021.08.18 等待接入
+                        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24_black);
+                        findViewById(R.id.habit_main_appbar).setBackgroundColor(ContextCompat.getColor(ActivityHabitMain.this,R.color.transparent));
+                        tabLayout.setVisibility(View.VISIBLE);
+
+                        getSupportActionBar().setTitle("");;
+                        replaceFragment(new FragmentHabitDoneRecordTable(getSupportActionBar(),tabLayout));
                         return true;
                 }
                 return false;
@@ -69,6 +88,7 @@ public class ActivityHabitMain extends BaseActivity {
 
     private void initView() {
         navigationView = findViewById(R.id.habit_main_bottom_navigation);
+        tabLayout = findViewById(R.id.habit_main_tab_layout);
     }
 
     private void replaceFragment(Fragment fragment){
@@ -76,5 +96,9 @@ public class ActivityHabitMain extends BaseActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.habit_main_fragment, fragment);
         transaction.commit();
+    }
+
+    private void configDoneRecordTable(){
+
     }
 }
---
+++ app/src/main/java/org/teamhavei/havei/fragments/FragmentHabitDoneRecordTable.java
@@ -0,0 +1,113 @@
+package org.teamhavei.havei.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import org.teamhavei.havei.Event.Habit;
+import org.teamhavei.havei.Event.HabitExec;
+import org.teamhavei.havei.Event.HaveIEvent;
+import org.teamhavei.havei.R;
+import org.teamhavei.havei.UniToolKit;
+import org.teamhavei.havei.activities.ActivityHabitDetail;
+import org.teamhavei.havei.adapters.TimeTablePageAdapter;
+import org.teamhavei.havei.databases.EventDBHelper;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class FragmentHabitDoneRecordTable extends BaseFragment {
+
+    EventDBHelper dbHelper;
+    ViewPager doneRecordVP;
+    ActionBar mActionBar;
+    TabLayout mTabLayout;
+    List<FragmentTimeTable> fragmentList = new ArrayList<>();
+    int weekOffset = 0;
+
+    public FragmentHabitDoneRecordTable(ActionBar actionBar, TabLayout tabLayout) {
+        this.mActionBar = actionBar;
+        this.mTabLayout = tabLayout;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: main");
+        View view = inflater.inflate(R.layout.fragment_habit_done_record_table, container, false);
+
+        dbHelper = new EventDBHelper(getContext(), EventDBHelper.DB_NAME, null, EventDBHelper.DB_VERSION);
+        initView(view);
+
+        Calendar startOfYear = Calendar.getInstance();
+        int curYear = startOfYear.get(Calendar.YEAR);
+        startOfYear.set(Calendar.DAY_OF_YEAR, 1);
+        /* 用于更新其他字段 */
+        startOfYear.getTime();
+        startOfYear.set(Calendar.DAY_OF_WEEK, 1);
+        /* 同上 */
+        startOfYear.getTime();
+
+        if (startOfYear.get(Calendar.YEAR) < curYear)
+            weekOffset = -1;
+
+        fragmentList.clear();
+        while (startOfYear.get(Calendar.YEAR) <= curYear) {
+
+            fragmentList.add(new FragmentTimeTable(startOfYear, new FragmentTimeTable.TimetableSocket() {
+                @Override
+                public void updateEventList(Calendar startOfWeek, List<FragmentTimeTable.TimeTableEvent> eventList) {
+                    eventList.clear();
+                    String sStartOfWeek = UniToolKit.eventDateFormatter(startOfWeek.getTime());
+                    startOfWeek.add(Calendar.DAY_OF_YEAR, 6);
+                    String sEndOfWeek = UniToolKit.eventDateFormatter(startOfWeek.getTime());
+                    startOfWeek.add(Calendar.DAY_OF_YEAR, -6);
+                    List<HabitExec> execList = dbHelper.findHabitExecByDateRange(sStartOfWeek, sEndOfWeek);
+                    int[] eventCount = new int[8];
+                    for (int i = 1; i <= 7; i++) {
+                        eventCount[i] = 0;
+                    }
+                    for (HabitExec exec : execList) {
+                        Habit habit = dbHelper.findHabitById(exec.getHabitId());
+                        Calendar startTime = Calendar.getInstance();
+                        startTime.setTime(UniToolKit.eventDateParser(exec.getDate()));
+                        startTime.set(Calendar.HOUR_OF_DAY, eventCount[startTime.get(Calendar.DAY_OF_WEEK)]++);
+                        eventList.add(new FragmentTimeTable.TimeTableEvent(startTime, habit.getName(), habit));
+                    }
+                }
+
+                @Override
+                public void onCardClick(HaveIEvent event) {
+                    ActivityHabitDetail.startAction(getActivity(), (Habit) event);
+                }
+            }, false));
+            startOfYear.add(Calendar.DAY_OF_YEAR, 7);
+        }
+
+        doneRecordVP.setAdapter(new TimeTablePageAdapter(getChildFragmentManager(), fragmentList, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT));
+        mTabLayout.setupWithViewPager(doneRecordVP);
+        for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+            mTabLayout.getTabAt(i).setText("第" + Integer.toString(i + 1) + "周");
+        }
+
+        mTabLayout.selectTab(mTabLayout.getTabAt(Calendar.getInstance().get(Calendar.WEEK_OF_YEAR) + weekOffset));
+
+        return view;
+    }
+
+
+    private void initView(View view) {
+        doneRecordVP = view.findViewById(R.id.habit_done_record_viewpager);
+    }
+}
